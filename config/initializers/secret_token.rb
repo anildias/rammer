0626643b53ea5078e8376a203a413db51a5eb636
manishaharidas <manisha@qburst.com>
2013-11-14 12:10:26 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RammerPortal::Application.config.secret_key_base = '95db140fdc3697e74218f47931b5bb6ac5ea517ecde6c0cb3acb07b09181ccd8fec8e91fffe79d4b8663a94172d23e1ce2e13dc1a1f6ce888329d96475ec11d8'
